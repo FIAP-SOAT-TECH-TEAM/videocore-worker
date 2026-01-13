399cbc6670a09c9875e0462f6cf804963cc61cc8
Pedro <ferrarezzolabs@outlook.com>
2026-01-12 21:03:30 -0300

    feat: create worker microsservice project using graal VM AOT

---
+++ worker/src/main/java/com/soat/fiap/videocore/worker/WorkerApplication.java
@@ -0,0 +1,13 @@
+package com.soat.fiap.videocore.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkerApplication.class, args);
+	}
+
+}
