ce8baa8902447d39c42a8fae7bb67c82f1d51b53
Pedro <ferrarezzolabs@outlook.com>
2026-01-16 11:06:13 -0300

    feat: implement logback appender to registry logback events as event of current span

--- worker/src/main/java/com/soat/fiap/videocore/worker/WorkerApplication.java
+++ worker/src/main/java/com/soat/fiap/videocore/worker/WorkerApplication.java
@@ -1,8 +1,14 @@
 package com.soat.fiap.videocore.worker;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication @Slf4j
 public class WorkerApplication {
@@ -11,5 +17,45 @@ public class WorkerApplication {
 
         SpringApplication.run(WorkerApplication.class, args);
 	}
+
+    @Bean
+    public CommandLineRunner testSendMetric(MeterRegistry meterRegistry) {
+        return args -> {
+            Counter counter = Counter.builder("videocore.test.metric")
+                    .description("Métrica de teste para validar envio OTLP")
+                    .tag("component", "startup")
+                    .tag("type", "manual")
+                    .register(meterRegistry);
+
+            counter.increment();
+            counter.increment(2);
+
+            log.info("Métrica videocore.test.metric enviada");
+        };
+    }
+
+
+    @Bean
+    public CommandLineRunner testSendTrace(Tracer tracer) {
+        return args -> {
+            Span span = tracer.nextSpan()
+                    .name("videocore.test.trace")
+                    .tag("component", "startup")
+                    .tag("type", "manual");
+
+            try (Tracer.SpanInScope scope = tracer.withSpan(span.start())) {
+                // Simula algum trabalho
+                Thread.sleep(100);
+                span.event("processing.started");
+                log.trace("processing.started começou");
+                span.event("processing.finished");
+            } catch (InterruptedException e) {
+                span.error(e);
+                Thread.currentThread().interrupt();
+            } finally {
+                span.end();
+            }
+        };
+    }
 
 }
