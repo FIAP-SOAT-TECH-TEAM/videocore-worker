02c6a42e645e18792ca618f2a4e5e002b0e772f6
Pedro <ferrarezzolabs@outlook.com>
2026-02-10 17:49:17 -0300

    chore: format terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "helm" {
   source = "./modules/helm"
 
   subscription_id                   = var.subscription_id
-  videocore-backend-container        = var.videocore-backend-container
-  videocore-backend-infra-key        = var.videocore-backend-infra-key
-  videocore-backend-resource-group   = var.videocore-backend-resource-group
-  videocore-backend-storage-account  = var.videocore-backend-storage-account
+  videocore-backend-container       = var.videocore-backend-container
+  videocore-backend-infra-key       = var.videocore-backend-infra-key
+  videocore-backend-resource-group  = var.videocore-backend-resource-group
+  videocore-backend-storage-account = var.videocore-backend-storage-account
   release_name                      = var.release_name
   repository_url                    = local.repository_url
   chart_name                        = var.chart_name
