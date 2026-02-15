c5154a9828d49907603ea65ee0507ed2dffbf09c
Pedro <ferrarezzolabs@outlook.com>
2026-02-14 23:05:42 -0300

    fix: use @RegisterReflectionForBinding annotation to register jackson hints

--- worker/src/main/java/com/soat/fiap/videocore/worker/WorkerApplication.java
+++ worker/src/main/java/com/soat/fiap/videocore/worker/WorkerApplication.java
@@ -1,13 +1,16 @@
 package com.soat.fiap.videocore.worker;
 
 import com.soat.fiap.videocore.worker.common.hints.azure.BlobHints;
-import com.soat.fiap.videocore.worker.common.hints.jackson.JacksonHints;
+import com.soat.fiap.videocore.worker.core.interfaceadapters.dto.ProcessVideoStatusUpdateEventDto;
+import com.soat.fiap.videocore.worker.infrastructure.in.event.azsvcbus.payload.BlobCreatedCloudEventSchemaPayload;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
-@ImportRuntimeHints({BlobHints.class, JacksonHints.class})
+@ImportRuntimeHints(BlobHints.class)
+@RegisterReflectionForBinding({BlobCreatedCloudEventSchemaPayload.class, ProcessVideoStatusUpdateEventDto.class})
 public class WorkerApplication {
 
     static void main(String[] args) {
